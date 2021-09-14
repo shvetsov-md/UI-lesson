35546047f88e87fcb6ac12f6da23e746d788176c
shvetsov-md <shvetsov-md@dartit.ru>
2021-09-14 17:57:27 +0500

    1 commit

---
+++ VK project/VK project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK project
+//
+//  Created by Максим Швецов on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
